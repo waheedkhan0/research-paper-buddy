136238b806b8aad7f805e31057c8d33948bc7a74
Waheed Khan <77748937+waheedkhan0@users.noreply.github.com>
2023-04-10 11:56:24 +0500

    changing placeholder of search

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -11,9 +11,7 @@ const handler = async (req: Request): Promise<Response> => {
       apiKey: string;
       matches: number;
     };
-
     const input = query.replace(/\n/g, " ");
-
     const res = await fetch("https://api.openai.com/v1/embeddings", {
       headers: {
         "Content-Type": "application/json",
